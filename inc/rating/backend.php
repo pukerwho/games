a0f32bdd6c54db3cb56eb0db42a87931ae2799aa
Vladimir Samojlov <v-moi@ya.ru>
2019-11-20 07:49:47 +0200

    edit back

--- inc/rating/backend.php
+++ inc/rating/backend.php
@@ -13,43 +13,43 @@ function myRating() {
 	wp_enqueue_script( 'rating' );
 }
 
-function rating_category(){
-	global $wpdb;
+// function rating_category(){
+// 	global $wpdb;
   
-  $category_id = $_POST['catId'];
-  $category_rating_new = $_POST['categoryRatingNew'];
-  $category_rating_old = $_POST['categoryRatingOld'];
-  $category_rating_count = $_POST['categoryRatingCount'];
+//   $category_id = $_POST['catId'];
+//   $category_rating_new = $_POST['categoryRatingNew'];
+//   $category_rating_old = $_POST['categoryRatingOld'];
+//   $category_rating_count = $_POST['categoryRatingCount'];
 
-  $category_rating_count = $category_rating_count + 1;
-  $category_rating_old = $category_rating_old + $category_rating_new;
+//   $category_rating_count = $category_rating_count + 1;
+//   $category_rating_old = $category_rating_old + $category_rating_new;
 
-  $wpdb->update(
-		'wp_termmeta', 
-		array(
-			'meta_value' => $category_rating_old,
-		),
-		array(
-			'term_id' => $category_id,
-			'meta_key' => '_crb_cats_rating',
-		),
-	);
+//   $wpdb->update(
+// 		'wp_termmeta', 
+// 		array(
+// 			'meta_value' => $category_rating_old,
+// 		),
+// 		array(
+// 			'term_id' => $category_id,
+// 			'meta_key' => '_crb_cats_rating',
+// 		),
+// 	);
 
-	$wpdb->update(
-		'wp_termmeta', 
-		array(
-			'meta_value' => $category_rating_count,
-		),
-		array(
-			'term_id' => $category_id,
-			'meta_key' => '_crb_cats_rating_count',
-		),
-	);
+// 	$wpdb->update(
+// 		'wp_termmeta', 
+// 		array(
+// 			'meta_value' => $category_rating_count,
+// 		),
+// 		array(
+// 			'term_id' => $category_id,
+// 			'meta_key' => '_crb_cats_rating_count',
+// 		),
+// 	);
 
-	echo $category_rating_old;
-}
+// 	echo $category_rating_old;
+// }
 
-add_action('wp_ajax_rating_category_back', 'rating_category');
-add_action('wp_ajax_nopriv_rating_category_back', 'rating_category');
+// add_action('wp_ajax_rating_category_back', 'rating_category');
+// add_action('wp_ajax_nopriv_rating_category_back', 'rating_category');
 
-?>
+// ?>
